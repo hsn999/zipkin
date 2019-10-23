d2f1a85ba6fb827dad79b84b13c29f5ecb933203
hsn999 <h_sn999@126.com>
2019-10-23 23:43:28 +0800

    new

---
+++ app2/src/main/java/frontend/remote/HelloRemote.java
@@ -0,0 +1,14 @@
+package frontend.remote;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "hello-remote", url = "http://localhost:9000", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+
+    @RequestMapping(value = "/api")
+    String printDate(@RequestParam(name = "username", required = false) String username);
+
+}
